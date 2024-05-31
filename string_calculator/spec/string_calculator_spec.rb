54650e6395ba9af4394bca8ade2838b88047a86f
User <prasadhande@gmail.com>
2024-05-31 20:41:51 +0530

    Signed-off-by: User <prasadhande@gmail.com>

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe StringCalculator do
     it 'returns the number itself for a single number' do
       expect(StringCalculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
     
   end
 end
