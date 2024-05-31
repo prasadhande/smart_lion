f0c411284dbfc047372273dfe1f554fce99bae22
User <prasadhande@gmail.com>
2024-05-31 20:41:09 +0530

    Signed-off-by: User <prasadhande@gmail.com>

--- string_calculator/spec/string_calculator_spec.rb
+++ string_calculator/spec/string_calculator_spec.rb
@@ -8,5 +8,9 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("")).to eq(0)
     end
 
+    it 'returns the number itself for a single number' do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
+    
   end
 end
